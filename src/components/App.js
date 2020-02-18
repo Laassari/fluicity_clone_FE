c137db198cef3a0bba413b93ee3e18f6028e18f3
Noureddine Laassari <nlaassari@gmail.com>
2020-02-18 15:03:40 +0100

    restructure the app and setup react-router

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { Route, BrowserRouter } from 'react-router-dom';
+import Navbar from './Navbar/';
+import Home from '../pages/Home';
+import About from '../pages/About';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Container>
+        <Route path="/" exact component={Home} />
+        <Route path="/about" exact component={About} />
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+export default App;
